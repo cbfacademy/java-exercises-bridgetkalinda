e76a830cb4c8abda51dd940516db88847f5bfe10
bridgetkalinda <bridgetkalinda@gmail.com>
2023-08-03 19:42:24 +0100

    created new class called HelloWorld

--- exercises/getting-started/src/com/codingblackfemales/academy/HelloWorld.java
+++ exercises/getting-started/src/com/codingblackfemales/academy/HelloWorld.java
@@ -1,4 +1,4 @@
-
+package com.codingblackfemales.academy; //com/coding blackfemales/academy
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/App.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/App.java
@@ -2,6 +2,7 @@ package com.cbfacademy;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Sphere s = new Sphere(newRadius 5);
+        System.out.println(s.area);
     }
 }
